d2d531e182bad2cbc77fc78ab516c3c904f72daa
betul.s <betul.s.99@hotmail.com>
2021-04-30 12:16:24 +0300

    urunler
    6 adet container eklendi

--- lib/urunlerim.dart
+++ lib/urunlerim.dart
@@ -23,19 +23,131 @@ class _UrunlerState extends State<Urunler> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(8.0),
-                  color: Colors.redAccent,
-
-                  /*decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),*/
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(10.0)),
-                ),
-              ],
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        padding: EdgeInsets.all(10.0),
+                        margin: EdgeInsets.all(8.0),
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            Expanded(child: Image.asset('images/masa.jpg')),
+                            Container(
+                              margin: EdgeInsets.all(10.0),
+                              child: Text('Masa'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        margin: EdgeInsets.all(8.0),
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            Expanded(child: Image.asset('images/sandalye.jpg')),
+                            Container(
+                              margin: EdgeInsets.all(10.0),
+                              child: Text('Sandalye'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      margin: EdgeInsets.all(8.0),
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Expanded(child: Image.asset('images/koltuk.jpg')),
+                          Container(
+                            margin: EdgeInsets.all(10.0),
+                            child: Text('Koltuk'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      margin: EdgeInsets.all(8.0),
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Expanded(child: Image.asset('images/saksi.jpg')),
+                          Container(
+                            margin: EdgeInsets.all(10.0),
+                            child: Text('Saksı'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      margin: EdgeInsets.all(8.0),
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Expanded(child: Image.asset('images/lamba.jpg')),
+                          Container(
+                            margin: EdgeInsets.all(10.0),
+                            child: Text('Lambader'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      margin: EdgeInsets.all(8.0),
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Expanded(child: Image.asset('images/saat.jpg')),
+                          Container(
+                            margin: EdgeInsets.all(10.0),
+                            child: Text('Saat'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
